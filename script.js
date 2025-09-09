edb36ba51ec8015ec88a4b29600bafafb4c69681
Caio Victor Barros Uchoa <caio.uchoa1@aluno.ce.gov.br>
2025-09-09 16:53:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+
+    let movesGen1 = prompt("digite seu moves")
+    let pokemons = prompt("digite seu pokemon")
+    
+    let Charmander =`
+    1	Arranhar  NORMAL         |Físico     |DMG 40 | CHANC 100|
+    8	Cortina de fumaça NORMAL |Status	 |    -- | CHANC 100|
+    12	Sopro do Dragão	 DRAGÃO  |Especial   |DMG 60 | CHANC 100|
+    17	Presa de Fogo	FOGO     |Físico     |DMG 65 | CHANC 95 |
+    20	Barra	NORMAL	         |Físico     |DMG 70 | CHANC 100|
+    24	Lança-chamas	FOGO     |Especial   |DMG 90 | CHANC 100|
+    28	Rosto assustador NORMAL	 |Status	 |    -- | CHANC 100|
+    40	Blitz de sinalização FOGO|Físico	 |DMG 120| CHANC 100|
+    `
+    switch (pokemons) {
+        case &{Charmander}
+    }
+
+    switch (movesGen1)
+
+      {
+        case "Flamethrower":
+    alert(`Tipo FOGO
+          Categoria	Especial   
+          Especial Poder 90 
+          Precisão 100  
+          PP 15   (máx. 24)
+          aprendem:Charmander`)
+    }
+     /*   
+      {
+        nome: "Hydro Pump",
+        tipo: "Water",
+        descricao: "Um jato de água em alta pressão com grande poder de ataque.",
+        pokemons: ["Blastoise", "Lapras", "Gyarados"]
+      },
+      {
+        nome: "Thunderbolt",
+        tipo: "Electric",
+        descricao: "Um raio elétrico que pode paralisar o oponente.",
+        pokemons: ["Pikachu", "Raichu", "Zapdos"]
+      },
+      {
+        nome: "Psychic",
+        tipo: "Psychic",
+        descricao: "Um ataque psíquico forte que pode baixar a defesa especial.",
+        pokemons: ["Alakazam", "Mewtwo", "Hypno"]
+      },
+      {
+        nome: "Body Slam",
+        tipo: "Normal",
+        descricao: "Um ataque fisicamente forte que pode paralisar o alvo.",
+        pokemons: ["Snorlax", "Rhydon", "Kangaskhan"]
+      }*/
