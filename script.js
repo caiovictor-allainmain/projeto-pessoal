048a7f9cb686af8292ed97d29c184776901aa4af
Caio Victor Barros Uchoa <caio.uchoa1@aluno.ce.gov.br>
2025-09-11 14:18:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
+let movesGen1 = prompt("digite seu moves")
+    switch (movesGen1){
 
-    let movesGen1 = prompt("digite seu moves")
-    let pokemons = prompt("digite seu pokemon")
-    
-    let Charmander =`
-    1	Arranhar  NORMAL         |Físico     |DMG 40 | CHANC 100|
-    8	Cortina de fumaça NORMAL |Status	 |    -- | CHANC 100|
-    12	Sopro do Dragão	 DRAGÃO  |Especial   |DMG 60 | CHANC 100|
-    17	Presa de Fogo	FOGO     |Físico     |DMG 65 | CHANC 95 |
-    20	Barra	NORMAL	         |Físico     |DMG 70 | CHANC 100|
-    24	Lança-chamas	FOGO     |Especial   |DMG 90 | CHANC 100|
-    28	Rosto assustador NORMAL	 |Status	 |    -- | CHANC 100|
-    40	Blitz de sinalização FOGO|Físico	 |DMG 120| CHANC 100|
-    `
-    switch (pokemons) {
-        case &{Charmander}
-    }
-
-    switch (movesGen1)
-
-      {
+      
         case "Flamethrower":
     alert(`Tipo FOGO
           Categoria	Especial   
           Especial Poder 90 
           Precisão 100  
           PP 15   (máx. 24)
-          aprendem:Charmander`)
+          efeitos:`)
     }
      /*   
       {
