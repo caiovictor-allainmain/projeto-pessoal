bf045917a7f28d6a288718431842dc91b5fe7ae7
Caio Victor Barros Uchoa <caio.uchoa1@aluno.ce.gov.br>
2025-09-11 14:37:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,57 @@
-let movesGen1 = prompt("digite seu moves")
+ let movesGen1 = prompt("digite seu moves")
     switch (movesGen1){
 
       
-        case "Flamethrower":
-    alert(`Tipo FOGO
-          Categoria	Especial   
-          Especial Poder 90 
-          Precisão 100  
-          PP 15   (máx. 24)
-          efeitos:`)
-    }
-     /*   
-      {
-        nome: "Hydro Pump",
-        tipo: "Water",
-        descricao: "Um jato de água em alta pressão com grande poder de ataque.",
-        pokemons: ["Blastoise", "Lapras", "Gyarados"]
-      },
-      {
-        nome: "Thunderbolt",
-        tipo: "Electric",
-        descricao: "Um raio elétrico que pode paralisar o oponente.",
-        pokemons: ["Pikachu", "Raichu", "Zapdos"]
-      },
-      {
-        nome: "Psychic",
-        tipo: "Psychic",
-        descricao: "Um ataque psíquico forte que pode baixar a defesa especial.",
-        pokemons: ["Alakazam", "Mewtwo", "Hypno"]
-      },
-      {
-        nome: "Body Slam",
-        tipo: "Normal",
-        descricao: "Um ataque fisicamente forte que pode paralisar o alvo.",
-        pokemons: ["Snorlax", "Rhydon", "Kangaskhan"]
-      }*/
+case "Flamethrower":
+alert(`Tipo FOGO
+Categoria	Especial   
+Especial Poder 90 
+Precisão 100  
+PP 15   (máx. 24)
+efeitos:
+O lança-chamas causa dano e tem 10% de chance de queimar o alvo.`)
+break
+case "Hydro Pump":
+alert(`Tipo	Água
+Categoria	Especial  Especial
+Poder	110
+Precisão	80
+PP	5   (máx. 8)
+Faz contato?	Não
+Introduzido	Geração 1
+Efeitos:
+Hydro Pump causa dano sem efeito adicional.`)
+break
+case "Thunderbolt":
+alert(`Tipo	Elétrico
+Categoria	Especial  Especial
+Poder	90
+Precisão	100
+PP	15   (máx. 24)
+Faz contato?	Não
+Introduzido	Geração 1)
+Efeitos:
+O raio causa dano e tem 10% de chance de paralisar o alvo.`)
+break
+case "Psychic":
+alert(`Tipo	Psíquico
+Categoria	Especial  Especial
+Poder	90
+Precisão	100
+PP	10   (máx. 16)
+Faz contato?	Não
+Introduzido	Geração 1
+Efeitos:
+Psíquico causa dano e tem 10% de chance de reduzir a Defesa Especial do alvo em um estágio.`)
+break
+case "Body Slam":
+alert(`Tipo	Normal
+Categoria	Físico  Físico
+Poder	85
+Precisão	100
+PP	15   (máx. 24)
+Faz contato?	Sim
+Introduzido	Geração 1
+Efeitos:
+Body Slam causa dano e tem 30% de chance de paralisar o alvo.`)
+}
