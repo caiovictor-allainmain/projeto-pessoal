a23ed23f922c683e209785c05ab6be6f3ae342a6
Caio Victor Barros Uchoa <caio.uchoa1@aluno.ce.gov.br>
2025-09-11 14:52:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
- let movesGen1 = prompt("digite seu moves")
+ 
+   let movesGen1 = prompt("digite seu moves")
     switch (movesGen1){
 
       
@@ -54,4 +55,77 @@ Faz contato?	Sim
 Introduzido	Geração 1
 Efeitos:
 Body Slam causa dano e tem 30% de chance de paralisar o alvo.`)
-}
+break
+case"Absorb":
+alert(`Tipo	Grama
+Categoria	Especial  Especial
+Poder	20
+Precisão	100
+PP	25   (máx. 40)
+Faz contato?	Não
+Introduzido	Geração 1
+Efeitos:
+Absorver causa dano e o usuário recupera 50% do HP drenado.
+`)
+break
+case"Acid":
+alert(`Tipo	Tóxico
+Categoria	Especial  Especial
+Poder	40
+Precisão	100
+PP	30   (máx. 48)
+Faz contato?	Não
+Introduzido	Geração 1
+Efeitos:
+O ácido causa dano e tem 10% de chance de reduzir a Defesa Especial do alvo em um estágio.`)
+break
+case"Sharpen":
+alert(`Tipo	Normal
+Categoria	Status  Status
+Poder	—
+Precisão	—
+PP	30   (máx. 48)
+Faz contato?	Não
+Introduzido	Geração 1
+Efeitos:
+Afiar aumenta o Ataque do usuário em um estágio.`)
+break
+case"Agility":
+alert(`Tipo	Psíquico
+Categoria	Status  Status
+Poder	—
+Precisão	—
+PP	30   (máx. 48)
+Faz contato?	Não
+Introduzido	Geração 1
+Efeitos:
+Agilidade aumenta a Velocidade do usuário em dois estágios.`)
+break
+case"Amnesia":
+alert(`Tipo	Psíquico
+Categoria	Status  Status
+Poder	—
+Precisão	—
+PP	20   (máx. 32)
+Faz contato?	Não
+Introduzido	Geração 1
+Efeitos:
+Amnésia aumenta a Defesa Especial do usuário em dois estágios.
+As estatísticas podem ser aumentadas até um máximo de +6 estágios cada.`)
+break
+case"":
+break
+case"":
+break
+case"":
+break
+case"":
+break
+case"":
+break
+case"":
+break
+case"":
+break
+case"":}
+
