18ae1fd2704ba03f42e67514dcb6be314f8aa8e8
Caio Victor Barros Uchoa <caio.uchoa1@aluno.ce.gov.br>
2025-09-16 11:50:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -113,19 +113,281 @@ Efeitos:
 Amnésia aumenta a Defesa Especial do usuário em dois estágios.
 As estatísticas podem ser aumentadas até um máximo de +6 estágios cada.`)
 break
-case"":
+case"Aurora Beam":
+alert(`Tipo	Gelo
+Categoria	Especial  Especial
+Poder	65
+Precisão	100
+PP	20   (máx. 32)
+Faz contato?	Não
+Introduzido	Geração 1`)
+break
+case"Barrage":
+alert(`Tipo	Normal
+Categoria	Físico  Físico
+Poder	15
+Precisão	85
+PP	20   (máx. 32)
+Faz contato?	Não
+Introduzido	Geração 1`)
+break
+case"Barrier":
+alert(`Tipo	Psíquico
+Categoria	Status  Status
+Poder	—
+Precisão	—
+PP	20   (máx. 32)
+Faz contato?	Não
+Introduzido	Geração 1`)
+break
+case"Bide":
+alert(`Tipo	Normal
+Categoria	Físico  Físico
+Poder	—
+Precisão	—
+PP	10   (máx. 16)
+Prioridade	+1
+Faz contato?	Sim
+Introduzido	Geração 1`)
+break
+case"Bind":
+alert(`Tipo	Normal
+Categoria	Físico  Físico
+Poder	15
+Precisão	85
+PP	20   (máx. 32)
+Faz contato?	Sim
+Introduzido	Geração 1`)
+break
+case"Bite":
+alert(`Tipo	Escuro
+Categoria	Físico  Físico
+Poder	60
+Precisão	100
+PP	25   (máx. 40)
+Faz contato?	Sim
+Introduzido	Geração 1`)
+break
+case"Blizzard":
+alert(`Tipo	Gelo
+Categoria	Especial  Especial
+Poder	110
+Precisão	70
+PP	5   (máx. 8)
+Faz contato?	Não
+Introduzido	Geração 1`)
+break
+case"Body Slam":
+alert(`Tipo	Normal
+Categoria	Físico  Físico
+Poder	85
+Precisão	100
+PP	15   (máx. 24)
+Faz contato?	Sim
+Introduzido	Geração 1`)
+break
+case"Bone Club":
+alert(`Tipo	Chão
+Categoria	Físico  Físico
+Poder	65
+Precisão	85
+PP	20   (máx. 32)
+Faz contato?	Não
+Introduzido	Geração 1`)
+break
+case"Bonemerang":
+alert(`Tipo	Chão
+Categoria	Físico  Físico
+Poder	50
+Precisão	90
+PP	10   (máx. 16)
+Faz contato?	Não
+Introduzido	Geração 1`)
+break
+case"Bubble":
+alert(`Tipo	Água
+Categoria	Especial  Especial
+Poder	40
+Precisão	100
+PP	30   (máx. 48)
+Faz contato?	Não
+Introduzido	Geração 1`)
+break
+case"Bubble Beam":
+alert(`Tipo	Água
+Categoria	Especial  Especial
+Poder	65
+Precisão	100
+PP	20   (máx. 32)
+Faz contato?	Não
+Introduzido	Geração 1`)
+break
+case"Clamp":
+alert(`Tipo	Água
+Categoria	Físico  Físico
+Poder	35
+Precisão	85
+PP	15   (máx. 24)
+Faz contato?	Sim
+Introduzido	Geração 1`)
+break
+case"Comet Punch":
+alert(`Tipo	Normal
+Categoria	Físico  Físico
+Poder	18
+Precisão	85
+PP	15   (máx. 24)
+Faz contato?	Sim
+Introduzido	Geração 1`)
+break
+case"Confuse Ray":
+alert(`Tipo	Fantasma
+Categoria	Status  Status
+Poder	—
+Precisão	100
+PP	10   (máx. 16)
+Faz contato?	Não
+Introduzido	Geração 1`)
+break
+case"Confusion":
+alert(`Tipo	Psíquico
+Categoria	Especial  Especial
+Poder	50
+Precisão	100
+PP	25   (máx. 40)
+Faz contato?	Não
+Introduzido	Geração 1`)
+break
+case"Constrict":
+alert(`Tipo	Normal
+Categoria	Físico  Físico
+Poder	10
+Precisão	100
+PP	35   (máx. 56)
+Faz contato?	Sim
+Introduzido	Geração 1`)
+break
+case"Conversion":
+alert(`Tipo	Normal
+Categoria	Status  Status
+Poder	—
+Precisão	—
+PP	30   (máx. 48)
+Faz contato?	Não
+Introduzido	Geração 1`)
+break
+case"Counter":
+alert(`Tipo	Combate
+Categoria	Físico  Físico
+Poder	—
+Precisão	100
+PP	20   (máx. 32)
+Prioridade	-5
+Faz contato?	Sim
+Introduzido	Geração 1`)
+break
+case"Crabhammer":
+alert(`Tipo	Água
+Categoria	Físico  Físico
+Poder	100
+Precisão	90
+PP	10   (máx. 16)
+Faz contato?	Sim
+Introduzido	Geração 1`)
+break
+case"Cut":
+alert(`
+Tipo	Normal
+Categoria	Físico  Físico
+Poder	50
+Precisão	95
+PP	30   (máx. 48)
+Faz contato?	Sim
+Introduzido	Geração 1`)
+break
+case"Defense Curl":
+alert(`Tipo	Normal
+Categoria	Status  Status
+Poder	—
+Precisão	—
+PP	40   (máx. 64)
+Faz contato?	Não
+Introduzido	Geração 1`)
+break
+case"Dig":
+alert(`Tipo	Chão
+Categoria	Físico  Físico
+Poder	80
+Precisão	100
+PP	10   (máx. 16)
+Faz contato?	Sim
+Introduzido	Geração 1`)
+break
+case"Disable":
+alert(`Tipo	Normal
+Categoria	Status  Status
+Poder	—
+Precisão	100
+PP	20   (máx. 32)
+Faz contato?	Não
+Introduzido	Geração 1`)
+break
+case"Dizzy Punch":
+alert(`Tipo	Normal
+Categoria	Físico  Físico
+Poder	70
+Precisão	100
+PP	10   (máx. 16)
+Faz contato?	Sim
+Introduzido	Geração 1`)
+break
+case"Double Kick":
+alert(`Tipo	Combate
+Categoria	Físico  Físico
+Poder	30
+Precisão	100
+PP	30   (máx. 48)
+Faz contato?	Sim
+Introduzido	Geração 1`)
+break
+case"Double Slap":
+alert(`Tipo	Normal
+Categoria	Físico  Físico
+Poder	15
+Precisão	85
+PP	10   (máx. 16)
+Faz contato?	Sim
+Introduzido	Geração 1`)
+break
+case"Double Team":
+alert(`Tipo	Normal
+Categoria	Status  Status
+Poder	—
+Precisão	—
+PP	15   (máx. 24)
+Faz contato?	Não
+Introduzido	Geração 1`)
+break
+case"Double-Edge":
+alert(`Tipo	Normal
+Categoria	Physical  Físico
+Poder	120
+Precisão	100
+PP	15   (máx. 24)
+Faz contato?	Sim
+Introduzido	Geração 1`)
+break
+case"Dragon Rage":
+alert(`Tipo	Dragão
+Categoria	Especial  Especial
+Poder	—
+Precisão	100
+PP	10   (máx. 16)
+Faz contato?	Não
+Introduzido	Geração 1`)
 break
 case"":
 break
 case"":
-break
-case"":
-break
-case"":
-break
-case"":
-break
-case"":
-break
-case"":}
+}
 
